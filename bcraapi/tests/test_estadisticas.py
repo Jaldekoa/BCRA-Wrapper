102f982d2c90f6f4ae9367e297fa21a721e3f3ab
Jon Aldekoa <jaldekoa@gmail.com>
2024-08-08 00:33:30 +0200

    Fix test

--- bcraapi/tests/test_estadisticas.py
+++ bcraapi/tests/test_estadisticas.py
@@ -4,7 +4,8 @@ import pandas as pd
 import pytest
 
 variable_ids = principales_variables()["idVariable"].tolist()
-
+# 21,25,7919,"M2 privado, promedio móvil de 30 días, variación interanual (en %)",2021-12-31,54.4
+variable_ids.remove(25)  # Error: No recent data
 
 def test_principales_variables():
     df = principales_variables()
@@ -14,4 +15,4 @@ def test_principales_variables():
 @pytest.mark.parametrize("id_variable", variable_ids)
 def test_datos_variable(id_variable):
     df = datos_variable(id_variable, "2024-01-01", f"{datetime.today():%Y-%m-%d}")
-    assert isinstance(df, pd.DataFrame) and not df.empty
+    assert (isinstance(df, pd.DataFrame) and not df.empty) or id_variable == 25
