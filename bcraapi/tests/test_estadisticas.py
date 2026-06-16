3baf9b3e65349fee7ff3aa66841f77ff0d1cff55
Jon Aldekoa <jaldekoa@gmail.com>
2026-06-16 23:32:37 +0200

    Remove not empty conditional because metodologia or datos_monetarios could be empty

--- bcraapi/tests/test_estadisticas.py
+++ bcraapi/tests/test_estadisticas.py
@@ -21,10 +21,10 @@ def test_metodologia_void():
 @pytest.mark.parametrize("id_variable", valores_aleatorios)
 def test_datos_monetarias(id_variable):
     df = datos_monetarias(id_variable, hasta=f"{datetime.today():%Y-%m-%d}")
-    assert (isinstance(df, pd.DataFrame) and not df.empty)
+    assert isinstance(df, pd.DataFrame)
 
 
 @pytest.mark.parametrize("id_variable", valores_aleatorios)
 def test_metodologia(id_variable):
     df = metodologia(id_variable)
-    assert isinstance(df, pd.DataFrame) and not df.empty
+    assert isinstance(df, pd.DataFrame)
